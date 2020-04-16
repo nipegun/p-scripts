d316de665e88c686f826646e297ad28141e844d8
NiPeGun <nipegun@gmail.com>
2020-04-16 02:43:02 +0200

    Update CrearAutoridadCertificadora.sh

--- ÚnicaEjecución/CrearAutoridadCertificadora.sh
+++ ÚnicaEjecución/CrearAutoridadCertificadora.sh
@@ -13,12 +13,12 @@ wget --no-check-certificate -P /root/ https://github.com/OpenVPN/easy-rsa/releas
 tar xvf /root/EasyRSA-unix-v3.0.6.tgz -C /root/
 mv /root/EasyRSA-v3.0.6 /root/EasyRSA
 cp /root/EasyRSA/vars.example /root/EasyRSA/vars
-sed -i -e 's|#set_var EASYRSA_REQ_COUNTRY    "US"|set_var EASYRSA_REQ_COUNTRY    "ES"|g' /root/EasyRSA/vars
-sed -i -e 's|#set_var EASYRSA_REQ_PROVINCE   "California"|set_var EASYRSA_REQ_PROVINCE   "Madrid"|g' /root/EasyRSA/vars
-sed -i -e 's|#set_var EASYRSA_REQ_CITY       "San Francisco"|set_var EASYRSA_REQ_CITY       "Madrid"|g' /root/EasyRSA/vars
-sed -i -e 's|#set_var EASYRSA_REQ_ORG        "Copyleft Certificate Co"|set_var EASYRSA_REQ_ORG        "Empresa"|g' /root/EasyRSA/vars
-sed -i -e 's|#set_var EASYRSA_REQ_EMAIL      "me@example.net"|set_var EASYRSA_REQ_EMAIL      "mail@empresa.com"|g' /root/EasyRSA/vars
-sed -i -e 's|#set_var EASYRSA_REQ_OU         "My Organizational Unit"|set_var EASYRSA_REQ_OU         "Departamento"|g' /root/EasyRSA/vars
+sed -i -e 's|#set_var EASYRSA_REQ_COUNTRY    "US"|set_var EASYRSA_REQ_COUNTRY                          "ES"|g' /root/EasyRSA/vars
+sed -i -e 's|#set_var EASYRSA_REQ_PROVINCE   "California"|set_var EASYRSA_REQ_PROVINCE             "Madrid"|g' /root/EasyRSA/vars
+sed -i -e 's|#set_var EASYRSA_REQ_CITY       "San Francisco"|set_var EASYRSA_REQ_CITY              "Madrid"|g' /root/EasyRSA/vars
+sed -i -e 's|#set_var EASYRSA_REQ_ORG        "Copyleft Certificate Co"|set_var EASYRSA_REQ_ORG    "Empresa"|g' /root/EasyRSA/vars
+sed -i -e 's|#set_var EASYRSA_REQ_EMAIL      "me@example.net"|set_var EASYRSA_REQ_EMAIL  "mail@empresa.com"|g' /root/EasyRSA/vars
+sed -i -e 's|#set_var EASYRSA_REQ_OU         "My Organizational Unit"|set_var EASYRSA_REQ_OU "Departamento"|g' /root/EasyRSA/vars
 cd /root/EasyRSA/
 /root/EasyRSA/easyrsa init-pki
 /root/EasyRSA/easyrsa build-ca nopass
