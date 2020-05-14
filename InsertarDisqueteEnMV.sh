2ecfde663847066a0eee657d390219a2205ac98e
NiPeGun <nipegun@gmail.com>
2020-05-14 21:42:29 +0200

    Update InsertarDisqueteEnMV.sh

--- InsertarDisqueteEnMV.sh
+++ InsertarDisqueteEnMV.sh
@@ -16,7 +16,7 @@ echo ""
 echo "Do you wish to install this program?"
 select yn in "Yes" "No"; do
     case $yn in
-        Yes ) make install; break;;
+        Yes ) echo "Insertando disquete y reárrancando la MV..."; break;;
         No ) exit;;
     esac
 done
