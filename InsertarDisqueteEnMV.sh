f0456a4a59afb650b0006aab9a592265f6a85d39
NiPeGun <nipegun@gmail.com>
2020-05-14 21:34:39 +0200

    Update InsertarDisqueteEnMV.sh

--- InsertarDisqueteEnMV.sh
+++ InsertarDisqueteEnMV.sh
@@ -20,3 +20,12 @@ select sn in "Si" "No"; do
     esac
 done
 
+while true; do
+    read -p "Presiona [S] para insertar el disquete apagando la máquina o [N] para cancelar..." yn
+    case $yn in
+        [Yy]* ) comands; break;;
+        [Nn]* ) exit;;
+        * ) echo "Responde S o N";;
+    esac
+done
+
