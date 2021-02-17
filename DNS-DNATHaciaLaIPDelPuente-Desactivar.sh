63fdc096e8be9d82a2d5f10879add79e5734a835
NiPeGun <nipegun@gmail.com>
2021-02-17 07:10:28 +0100

    Create DNS-DNATHaciaLaIPDelPuente-Desactivar.sh

---
+++ DNS-DNATHaciaLaIPDelPuente-Desactivar.sh
@@ -0,0 +1,20 @@
+#!/bin/bash
+
+# Pongo a disposición pública este script bajo el término de "software de dominio público".
+# Puedes hacer lo que quieras con él porque es libre de verdad; no libre con condiciones como las licencias GNU y otras patrañas similares.
+# Si se te llena la boca hablando de libertad entonces hazlo realmente libre.
+# No tienes que aceptar ningún tipo de términos de uso o licencia para utilizarlo o modificarlo porque va sin CopyLeft.
+
+#--------------------------------------------------------------------------------------------------
+#  Script de NiPeGun para desactivar el reenvio de todo el tráfico DNS hacia la IP LAN del router
+#--------------------------------------------------------------------------------------------------
+
+# Borrar el archivo de reglas
+rm -rf /root/scripts/NFTables-DNS-DNATHaciaLaIPDelPuente.nft
+
+# Borrar la inclusión de la reglas en el archivo de configuración de NFTables
+sed -i -e 's|include "/root/scripts/NFTables-DNS-DNATHaciaLaIPLANDelPuente.nft"||g' /etc/nftables.conf
+
+# Recargar NFTables
+nft --file /etc/nftables.conf
+
