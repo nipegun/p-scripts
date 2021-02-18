2ac497f9699e2f5feada165b2993b6a015762dbf
NiPeGun <nipegun@gmail.com>
2021-02-18 04:40:03 +0100

    Update DNS-QueriesDelPuente-EnTiempoReal.sh

--- DNS-QueriesDelPuente-EnTiempoReal.sh
+++ DNS-QueriesDelPuente-EnTiempoReal.sh
@@ -16,9 +16,9 @@ ColorVerde='\033[1;32m'
 ColorFin='\033[0m'
 
 # Comprobar si el paquete tcpdump está instalado. Si no está, instalarlo.
-if [ $(dpkg-query -W -f='${Status}' tcpdump 2>/dev/null | grep -c "ok installed") -eq 0 ]; then
+if [[ $(dpkg-query -s tcpdump 2>/dev/null | grep installed) == "" ]]; then
     echo ""
-    echo "NFTables no está instalado. Se procederá a su instalación..."
+    echo "tcpdump no está instalado. Se procederá a su instalación..."
     echo ""
     apt-get -y update
     apt-get -y install tcpdump
