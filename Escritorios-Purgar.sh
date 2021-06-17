9d5e48eab22ac1f6c5a713800169defe7d68ac8d
NiPeGun <nipegun@gmail.com>
2021-06-17 19:46:07 +0200

    Create Escritorios-Purgar.sh

---
+++ Escritorios-Purgar.sh
@@ -0,0 +1,64 @@
+#!/bin/bash
+
+# Pongo a disposición pública este script bajo el término de "software de dominio público".
+# Puedes hacer lo que quieras con él porque es libre de verdad; no libre con condiciones como las licencias GNU y otras patrañas similares.
+# Si se te llena la boca hablando de libertad entonces hazlo realmente libre.
+# No tienes que aceptar ningún tipo de términos de uso o licencia para utilizarlo o modificarlo porque va sin CopyLeft.
+
+#-----------------------------------------------------------------------------
+#  Script de NiPeGun para borrar todos los entornos de escritorio de Proxmox
+#-----------------------------------------------------------------------------
+
+## XRDP
+   systemctl stop xrdp.service
+   apt-get -y purge xrdp
+   apt-get -y autoremove
+   tasksel remove desktop
+
+## cinnamon
+   tasksel remove cinnamon-desktop
+   apt-get -y purge cinnamon-session
+   apt-get -y autoremove --purge cinnamon*
+   apt-get -y autoremove
+
+## gnome
+   tasksel remove gnome-desktop
+   apt-get -y purge gnome-session
+   apt-get -y autoremove --purge gnome*
+   apt-get -y autoremove
+
+## kde
+   tasksel remove kde-desktop
+   apt-get -y purge kde-plasma-desktop
+   apt-get -y autoremove --purge kde*
+   apt-get -y autoremove --purge plasma*
+   apt-get -y autoremove
+
+## lxde
+   tasksel remove lxde-desktop
+   apt-get -y autoremove --purge lxde*
+   apt-get -y autoremove
+
+## lxqt
+   tasksel remove lxqt-desktop
+   apt-get -y purge lxqt-session
+   apt-get -y autoremove --purge lxqt*
+   apt-get -y autoremove
+
+## mate
+   tasksel remove mate-desktop
+   apt-get -y purge mate-session-manager
+   apt-get -y autoremove --purge mate*
+   apt-get -y autoremove
+
+## xfce
+   tasksel remove xfce-desktop
+   apt-get -y autoremove --purge xfce*
+
+## Gestores de ventanas
+   apt-get -y purge gdm3
+
+## Re-Instalar lo necesario
+   tasksel install mate-desktop
+   apt-get -y install xrdp
+
