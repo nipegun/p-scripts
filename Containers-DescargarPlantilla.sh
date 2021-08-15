8ad57c8e9c7de69c0f8d1194af1255e1903e8125
NiPeGun <nipegun@gmail.com>
2021-08-16 01:53:29 +0200

    Create Containers-DescargarPlantilla.sh

---
+++ Containers-DescargarPlantilla.sh
@@ -0,0 +1,39 @@
+#!/bin/bash
+
+# Pongo a disposición pública este script bajo el término de "software de dominio público".
+# Puedes hacer lo que quieras con él porque es libre de verdad; no libre con condiciones como las licencias GNU y otras patrañas similares.
+# Si se te llena la boca hablando de libertad entonces hazlo realmente libre.
+# No tienes que aceptar ningún tipo de términos de uso o licencia para utilizarlo o modificarlo porque va sin CopyLeft.
+
+#-----------------------------------------------------------------------
+#  Script de NiPeGun para descargar un container específico en Proxmox
+#-----------------------------------------------------------------------
+
+CantArgsEsperados=2
+ArgsInsuficientes=65
+
+ColorRojo='\033[1;31m'
+ColorVerde='\033[1;32m'
+FinColor='\033[0m'
+
+if [ $# -ne $CantArgsEsperados ]
+  then
+    echo ""
+    echo "-------------------------------------------------------------------------------"
+    echo -e "${ColorRojo}  Mal uso del script.${FinColor} El uso correcto sería:"
+    echo ""
+    echo -e "  $0 ${ColorVerde}[NombreDeLaPlantilla] [Almacenamiento>DeDestino]${FinColor}"
+    echo ""
+    echo "  Ejemplo:"
+    echo "  $0 debian-10-standard_10.7-1_amd64.tar.gz local"
+    echo ""
+    echo "  Para ver las plantillas de contenedores disponibles para descargar, ejecuta:"
+    echo "  pveam update && pveam available"
+    echo ""
+    echo "-------------------------------------------------------------------------------"
+    echo ""
+    exit $ArgsInsuficientes
+  else
+    pveam download $2 $1
+fi
+
