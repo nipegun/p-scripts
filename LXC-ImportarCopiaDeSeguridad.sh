f83efee20b0fc3331e1180c2b36c96a5a342b29c
NiPeGun <nipegun@gmail.com>
2021-09-23 15:54:52 +0200

    Create LXC-ImportarCopiaDeSeguridad.sh

---
+++ LXC-ImportarCopiaDeSeguridad.sh
@@ -0,0 +1,36 @@
+#!/bin/bash
+
+# Pongo a disposición pública este script bajo el término de "software de dominio público".
+# Puedes hacer lo que quieras con él porque es libre de verdad; no libre con condiciones como las licencias GNU y otras patrañas similares.
+# Si se te llena la boca hablando de libertad entonces hazlo realmente libre.
+# No tienes que aceptar ningún tipo de términos de uso o licencia para utilizarlo o modificarlo porque va sin CopyLeft.
+
+#----------------------------------------------------------------------------
+#  Script de NiPeGun para importar una copia de seguridad de contenedor LXC
+#----------------------------------------------------------------------------
+
+CantArgsEsperados=3
+ArgsInsuficientes=65
+
+ColorAdvertencia='\033[1;31m'
+ColorArgumentos='\033[1;32m'
+FinColor='\033[0m'
+
+if [ $# -ne $CantArgsEsperados ]
+  then
+    echo ""
+    echo "------------------------------------------------------------------------------"
+    echo -e "${ColorAdvertencia}Mal uso del script.${FinColor} El uso correcto sería:"
+    echo ""
+    echo -e "$0 ${ColorArgumentos}[IDDelNuevoContainer] [RutaAbsolutaAlArchivo] [Almacenamiento]${FinColor}"
+    echo ""
+    echo "Ejemplo:"
+    echo "$0 130 /root/copias/vzdump-lxc-777.tar local-lvm" 
+    echo "------------------------------------------------------------------------------"
+    echo ""
+    exit $ArgsInsuficientes
+  else
+    echo ""
+    pct restore $1 $2 -storage $3
+    echo ""
+fi
