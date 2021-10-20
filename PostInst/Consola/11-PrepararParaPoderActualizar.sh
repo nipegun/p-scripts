b5ed7ad65506e1e24075ad1c4058195e4fb57fcb
NiPeGun <nipegun@gmail.com>
2021-10-20 19:59:41 +0200

    Update 11-PrepararParaPoderActualizar.sh

--- PostInst/Consola/11-PrepararParaPoderActualizar.sh
+++ PostInst/Consola/11-PrepararParaPoderActualizar.sh
@@ -72,8 +72,20 @@ elif [ $OS_VERS == "9" ]; then
   echo ""
 
   echo ""
-  echo "  Comandos para ProxmoxVE 5 todavía no preparados. Prueba ejecutar el script en otra versión de ProxmoxVE."
+  echo -e "${ColorVerde}Deshabilitando el repositorio Enterprise...${FinColor}"
   echo ""
+  cp /etc/apt/sources.list.d/pve-enterprise.list /etc/apt/sources.list.d/pve-enterprise.list.bak
+  sed -i -e 's|deb https://enterprise.proxmox.com/debian/pve stretch pve-enterprise|# deb https://enterprise.proxmox.com/debian/pve stretch pve-enterprise|g' /etc/apt/sources.list.d/pve-enterprise.list
+
+  echo ""
+  echo -e "${ColorVerde}Agregando el repositorio para no-suscriptores...${FinColor}"
+  echo ""
+  echo "deb http://download.proxmox.com/debian/pve stretch pve-no-subscription" > /etc/apt/sources.list.d/pve-no-sub.list
+
+  echo ""
+  echo -e "${ColorVerde}Activando cambios en apt...${FinColor}"
+  echo ""
+  apt-get -y update
 
 elif [ $OS_VERS == "10" ]; then
 
