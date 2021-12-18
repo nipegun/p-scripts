0ffe4aeadd93566f03dc855ee4e71013d5437af5
NiPeGun <nipegun@gmail.com>
2021-12-18 22:05:23 +0100

    Update Kernel-MostrarInstalados.sh

--- Kernel-MostrarInstalados.sh
+++ Kernel-MostrarInstalados.sh
@@ -5,15 +5,16 @@
 # Si se te llena la boca hablando de libertad entonces hazlo realmente libre.
 # No tienes que aceptar ningún tipo de términos de uso o licencia para utilizarlo o modificarlo porque va sin CopyLeft.
 
-#--------------------------------------------------------------------
+#-------------------------------------------------------------------------------------------------------
 #  Script de NiPeGun para mostrar los kernels instalados en ProxmoxVE
 #
 #  Ejecución remota:
-#  
+#  curl -s https://raw.githubusercontent.com/nipegun/p-scripts/master/Kernel-MostrarInstalados.sh | bash
 #
-#--------------------------------------------------------------------
+#-------------------------------------------------------------------------------------------------------
 
 echo ""
 echo "  Kernels instalados en este servidor Proxmox:"
 echo ""
 dpkg-query -l | grep pve-kernel | cut -d ' ' -f3 | grep -v firmware | grep -v helper
+
