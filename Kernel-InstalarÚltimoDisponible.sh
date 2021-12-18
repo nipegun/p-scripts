5a1e806ecccbb81d09ca5b408521bcbde79bdd11
NiPeGun <nipegun@gmail.com>
2021-12-18 20:51:23 +0100

    Update and rename Kernel-InstalarÚltimoDIsponible.sh to Kernel-InstalarÚltimoDisponible.sh

--- Kernel-InstalarÚltimoDisponible.sh
+++ Kernel-InstalarÚltimoDisponible.sh
@@ -5,9 +5,13 @@
 # Si se te llena la boca hablando de libertad entonces hazlo realmente libre.
 # No tienes que aceptar ningún tipo de términos de uso o licencia para utilizarlo o modificarlo porque va sin CopyLeft.
 
-#----------------------------------------------------------------------------------
+#-------------------------------------------------------------------------------------------------------------------
 #  Script de NiPeGun para instalar el último kernel disponible en ProxmoxVExmoxVE
-#----------------------------------------------------------------------------------
+#
+#  Ejecución remota:
+#  curl -s https://raw.githubusercontent.com/nipegun/p-scripts/master/Kernel-Instalar%C3%9AltimoDisponible.sh | bash
+#
+#-------------------------------------------------------------------------------------------------------------------
 
 ColorRojo='\033[1;31m'
 ColorVerde='\033[1;32m'
