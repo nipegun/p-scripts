fa57676449095eedef815d3f59470795b644945a
NiPeGun <nipegun@gmail.com>
2022-01-29 20:12:17 +0100

    Update PVE-CopSegBD.sh

--- PVE-CopSegBD.sh
+++ PVE-CopSegBD.sh
@@ -12,24 +12,44 @@
 #  curl -s https://raw.githubusercontent.com/nipegun/p-scripts/master/PVE-CopSegBD.sh | bash
 #---------------------------------------------------------------------------------------------
 
-EstadoDeLaBaseDeDatosDePVE=$(sqlite3 /var/lib/pve-cluster/config.db "PRAGMA integrity_check;")
-if [ $EstadoDeLaBaseDeDatosDePVE == "ok" ]; then
-  echo ""
-  echo "  El estado de la base de datos es consistente. Procediendo con la copia de seguridad..."
-  echo ""
-  rm -f /var/lib/pve-cluster/config.db.sql 2> /dev/null
-  rm -f /var/lib/pve-cluster/config.db.bak 2> /dev/null
-  sqlite3 /var/lib/pve-cluster/config.db ".dump" | sqlite3 /var/lib/pve-cluster/config.db.bak
-  sqlite3 /var/lib/pve-cluster/config.db.bak ".mode insert" ".output /var/lib/pve-cluster/config.db.sql.tmp1" ".dump" | sqlite3 /var/lib/pve-cluster/config.db.bak
-  sqlformat --keywords upper --identifiers lower /var/lib/pve-cluster/config.db.sql.tmp1 > /var/lib/pve-cluster/config.db.sql.tmp2
-  cat /var/lib/pve-cluster/config.db.sql.tmp2 | sed 's/tree (/tree (\n/g' | sed 's/VALUES(/VALUES(\n  /g' | sed 's-,-,\n  -g'  | sed 's-);-\n);\n-g' | sed 's-    -  -g' > /var/lib/pve-cluster/config.db.sql
-  rm -f /var/lib/pve-cluster/config.db.sql.tmp1 2> /dev/null
-  rm -f /var/lib/pve-cluster/config.db.sql.tmp2 2> /dev/null
-else
-  echo ""
-  echo "  El estado de la base de datos no es consistente. Intentando exportar lo que se pueda..."
-  echo ""
-  sqlite3 /var/lib/pve-cluster/config.db ".recover" | sqlite3 /var/lib/pve-cluster/config.db.recover
-  sqlite3 /var/lib/pve-cluster/config.db ".dump" | sed -e 's|^ROLLBACK;\( -- due to errors\)*$|COMMIT;|g' | sqlite3 /var/lib/pve-cluster/config.db.dump-before-rollback
-fi
+ColorRojo='\033[1;31m'
+ColorVerde='\033[1;32m'
+FinColor='\033[0m'
+
+echo ""
+echo -e "${ColorVerde}-----------------------------------------------------------------------${FinColor}"
+echo -e "${ColorVerde}  Iniciando la copia de seguridad de la base de datos de ProxmoxVE...${FinColor}"
+echo -e "${ColorVerde}-----------------------------------------------------------------------${FinColor}"
+echo ""
+
+## Definir la fecha de ejecución del script
+   FechaDeEjec=$(date +A%YM%mD%d@%T)
+
+## Crear la carpeta de copias de seguridad interna (en caso de que no exista
+   mkdir -p /CopSegInt/$FechaDeEjec/BD/ 2> /dev/null
+
+## Ejecutar copia de seguridad
+   EstadoDeLaBaseDeDatosDePVE=$(sqlite3 /var/lib/pve-cluster/config.db "PRAGMA integrity_check;")
+   if [ $EstadoDeLaBaseDeDatosDePVE == "ok" ]; then
+     echo ""
+     echo "  El estado de la base de datos es consistente. Procediendo con la copia de seguridad..."
+     echo ""
+     rm -f /var/lib/pve-cluster/config.db.sql 2> /dev/null
+     rm -f /var/lib/pve-cluster/config.db.bak 2> /dev/null
+     sqlite3 /var/lib/pve-cluster/config.db ".dump" | sqlite3 /var/lib/pve-cluster/config.db.bak
+     sqlite3 /var/lib/pve-cluster/config.db.bak ".mode insert" ".output /var/lib/pve-cluster/config.db.sql.tmp1" ".dump" | sqlite3 /var/lib/pve-cluster/config.db.bak
+     sqlformat --keywords upper --identifiers lower /var/lib/pve-cluster/config.db.sql.tmp1 > /var/lib/pve-cluster/config.db.sql.tmp2
+     cat /var/lib/pve-cluster/config.db.sql.tmp2 | sed 's/tree (/tree (\n/g' | sed 's/VALUES(/VALUES(\n  /g' | sed 's-,-,\n  -g'  | sed 's-);-\n);\n-g' | sed 's-    -  -g' > /var/lib/pve-cluster/config.db.sql
+     rm -f /var/lib/pve-cluster/config.db.sql.tmp1 2> /dev/null
+     rm -f /var/lib/pve-cluster/config.db.sql.tmp2 2> /dev/null
+     mv /var/lib/pve-cluster/config.db.bak /CopSegInt/$FechaDeEjec/BD/config.db
+     mv /var/lib/pve-cluster/config.db.sql /CopSegInt/$FechaDeEjec/BD/config.db.sql
+     echo "Este archivo debe ubicarse en /var/lib/pve-cluster/" > /CopSegInt/$FechaDeEjec/BD/UbicDelArchivoConfigDB.txt
+   else
+     echo ""
+     echo "  El estado de la base de datos no es consistente. Intentando exportar lo que se pueda..."
+     echo ""
+     sqlite3 /var/lib/pve-cluster/config.db ".recover" | sqlite3 /var/lib/pve-cluster/config.db.recover
+     sqlite3 /var/lib/pve-cluster/config.db ".dump" | sed -e 's|^ROLLBACK;\( -- due to errors\)*$|COMMIT;|g' | sqlite3 /var/lib/pve-cluster/config.db.dump-before-rollback
+   fi
 
