22b4e0e20b737cb82777611b4c2fefab3c7b7a5c
NiPeGun <nipegun@gmail.com>
2022-01-29 20:04:41 +0100

    Update PVE-CopSegBD.sh

--- PVE-CopSegBD.sh
+++ PVE-CopSegBD.sh
@@ -1,3 +1,17 @@
+#!/bin/bash
+
+# Pongo a disposición pública este script bajo el término de "software de dominio público".
+# Puedes hacer lo que quieras con él porque es libre de verdad; no libre con condiciones como las licencias GNU y otras patrañas similares.
+# Si se te llena la boca hablando de libertad entonces hazlo realmente libre.
+# No tienes que aceptar ningún tipo de términos de uso o licencia para utilizarlo o modificarlo porque va sin CopyLeft.
+
+#----------------------------------------------------------------------------------
+#  Script de NiPeGun para hacer copia de seguridad de la base de datos de Proxmox
+#
+#  Ejecución remota:
+#  curl -s x | bash
+#----------------------------------------------------------------------------------
+
 EstadoDeLaBaseDeDatosDePVE=$(sqlite3 /var/lib/pve-cluster/config.db "PRAGMA integrity_check;")
 if [ $EstadoDeLaBaseDeDatosDePVE == "ok" ]; then
   echo ""
