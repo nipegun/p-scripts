f71e11e4bb2d8c1b05b7eff059c41b8602a9253e
NiPeGun <nipegun@gmail.com>
2022-02-09 21:57:30 +0100

    Create LXC-CopiaDeSeguridad-Crear.sh

---
+++ LXC-CopiaDeSeguridad-Crear.sh
@@ -0,0 +1,42 @@
+#!/bin/bash
+
+# Pongo a disposición pública este script bajo el término de "software de dominio público".
+# Puedes hacer lo que quieras con él porque es libre de verdad; no libre con condiciones como las licencias GNU y otras patrañas similares.
+# Si se te llena la boca hablando de libertad entonces hazlo realmente libre.
+# No tienes que aceptar ningún tipo de términos de uso o licencia para utilizarlo o modificarlo porque va sin CopyLeft.
+
+#-------------------------------------------------------------------------------------------------------------
+#  Script de NiPeGun para importar una copia de seguridad de contenedor LXC
+#
+#  Ejecución remota:
+#  curl -s https://raw.githubusercontent.com/nipegun/p-scripts/master/LXC-CopiaDeSeguridad-Crear.sh | bash
+#-------------------------------------------------------------------------------------------------------------
+
+CantArgsEsperados=1
+ArgsInsuficientes=65
+
+ColorAdvertencia='\033[1;31m'
+ColorArgumentos='\033[1;32m'
+FinColor='\033[0m'
+
+if [ $# -ne $CantArgsEsperados ]
+  then
+    echo ""
+    echo "------------------------------------------------------------------------------"
+    echo -e "${ColorAdvertencia}Mal uso del script.${FinColor} El uso correcto sería:"
+    echo ""
+    echo -e "$0 ${ColorArgumentos}[IDDelContainer]${FinColor}"
+    echo ""
+    echo "Ejemplo:"
+    echo "$0 130" 
+    echo "------------------------------------------------------------------------------"
+    echo ""
+    exit $ArgsInsuficientes
+  else
+    echo ""
+    vzdump $1 --mode stop --compress gzip --tmpdir /var/tmp --dumpdir /var/tmp  $2 -storage
+    echo ""
+    echo "Copia de seguridad del container guardada en /var/tmp"
+    echo ""
+fi
+
