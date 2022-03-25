e7c8176a9323294ebfb68ec7eddb11712b0dd1cb
NiPeGun <nipegun@gmail.com>
2022-03-25 22:55:03 +0100

    Create PCIPassThrough-ComprobarSiPreparado.sh

---
+++ PCIPassThrough-ComprobarSiPreparado.sh
@@ -0,0 +1,30 @@
+#!/bin/bash
+
+# Pongo a disposición pública este script bajo el término de "software de dominio público".
+# Puedes hacer lo que te salga de los huevos con él porque es libre de verdad; no libre con condiciones como las licencias GNU y otras patrañas similares.
+# Si se te llena la boca hablando de libertad entonces hazlo realmente libre.
+# No tienes que aceptar ningún tipo de términos de uso o licencia para utilizarlo o modificarlo porque va sin CopyLeft.
+
+#------------------------------------------------------------------------------------------------------------------------------
+#  Script de NiPeGun para comprobar si Proxmox está preparado para pasar dispositivos PCI a las máquinas virtuales
+#
+#  Ejecución remota:
+#  curl -s https://raw.githubusercontent.com/nipegun/p-scripts/master/PCIPassThrough-ComprobarSiPreparado.sh | bash
+#------------------------------------------------------------------------------------------------------------------------------
+
+if -f /etc/modprobe.d/pci-passthrough.conf
+  echo ""
+  echo "  El archivo /etc/modprobe.d/pci-passthrough.conf existe"
+  echo ""
+  if $(cat /etc/modprobe.d/pci-passthrough.conf | grep unsafe) != '' then
+    echo ""
+    echo "  Parece que has permitido las interrupciones inseguras de Interrupt Remmaping."
+    echo ""
+  fi
+else
+  echo ""
+  echo "  El archivo /etc/modprobe.d/pci-passthrough.conf no existe."
+  echo "  No parece que hayas configurado pci-passthrough en el pasado."
+  echo ""
+fi
+
