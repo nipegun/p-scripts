366e2d1056f6dcc367007107a399078ecfa07d5f
NiPeGun <nipegun@gmail.com>
2022-03-27 13:41:10 +0200

    Update and rename LXC-ExpandirPartRootAlMax11.sh to LXC-ExpandirPartRootAlMax.sh

--- LXC-ExpandirPartRootAlMax.sh
+++ LXC-ExpandirPartRootAlMax.sh
@@ -54,28 +54,24 @@ if [ $# -ne $CantArgsEsperados ]
     # Desmontar el disco (Si es que está montado)
       echo ""
       echo -e "${ColorAzul}  Desmontando el disco del contendor (si es que está montado en el host de Proxmox)...${FinColor}"
-      echo "  orden: umount "$vCarpetaAlmacenamiento"images/$1/$vArchivoDeDisco"
       echo ""
       umount "$vCarpetaAlmacenamiento"images/$1/$vArchivoDeDisco
 
     # Forzar la aceptación de que el disco no está montado
       echo ""
       echo -e "${ColorAzul}  Forzando el bloque MMP a limpio...${FinColor}"
-      echo "  orden: tune2fs -f -E clear_mmp "$vCarpetaAlmacenamiento"images/$1/$vArchivoDeDisco"
       echo ""
       tune2fs -f -E clear_mmp "$vCarpetaAlmacenamiento"images/$1/$vArchivoDeDisco
 
     # Revisar y reparar el sistema de archivos del contenedor
       echo ""
       echo -e "${ColorAzul}  Revisando y reparando el sistema de archivos del contendor...${FinColor}"
-      echo "  orden: e2fsck -f "$vCarpetaAlmacenamiento"images/$1/$vArchivoDeDisco"
       echo ""
       e2fsck -y -f "$vCarpetaAlmacenamiento"images/$1/$vArchivoDeDisco
 
     # Realizar la expansión
       echo ""
       echo -e "${ColorAzul}  Efectuando la redimensión...${FinColor}"
-      echo "  orden: resize2fs "$vCarpetaAlmacenamiento"images/$1/$vArchivoDeDisco"
       echo ""
       resize2fs "$vCarpetaAlmacenamiento"images/$1/$vArchivoDeDisco
 
