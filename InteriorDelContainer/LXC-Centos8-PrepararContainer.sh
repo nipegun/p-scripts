38498acc615b37cc03d6fada495ec005975f8c7c
NiPeGun <nipegun@gmail.com>
2022-05-08 21:24:51 +0200

    Create LXC-Centos8-PrepararContainer.sh

---
+++ InteriorDelContainer/LXC-Centos8-PrepararContainer.sh
@@ -0,0 +1,6 @@
+sed -i -e "s|mirrorlist=|#mirrorlist=|g" /etc/yum.repos.d/CentOS-*
+sed -i -e "s|#baseurl=http://mirror.centos.org|baseurl=http://vault.centos.org|g" /etc/yum.repos.d/CentOS-*
+yum -y update
+yum -y upgrade
+
+
