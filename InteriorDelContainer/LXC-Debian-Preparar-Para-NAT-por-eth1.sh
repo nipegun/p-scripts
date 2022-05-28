5cb9c9f0087640236b0f4c280b3a56e86f492516
NiPeGun <nipegun@gmail.com>
2022-05-28 19:23:04 +0200

    Create LXC-Debian-Preparar-Para-NAT-por-eth1.sh

---
+++ InteriorDelContainer/LXC-Debian-Preparar-Para-NAT-por-eth1.sh
@@ -0,0 +1,110 @@
+#!/bin/bash
+
+# Pongo a disposición pública este script bajo el término de "software de dominio público".
+# Puedes hacer lo que quieras con él porque es libre de verdad; no libre con condiciones como las licencias GNU y otras patrañas similares.
+# Si se te llena la boca hablando de libertad entonces hazlo realmente libre.
+# No tienes que aceptar ningún tipo de términos de uso o licencia para utilizarlo o modificarlo porque va sin CopyLeft.
+
+# ----------
+#  Script de NiPeGun para preparar un container LXC de Debian para natear
+#
+#  Ejecución remota:
+#  curl -s x | bash
+# ----------
+
+ColorRojo='\033[1;31m'
+ColorVerde='\033[1;32m'
+FinColor='\033[0m'
+
+# Determinar la versión de Debian
+
+  if [ -f /etc/os-release ]; then
+    # Para systemd y freedesktop.org
+      . /etc/os-release
+      OS_NAME=$NAME
+      OS_VERS=$VERSION_ID
+  elif type lsb_release >/dev/null 2>&1; then
+    # linuxbase.org
+      OS_NAME=$(lsb_release -si)
+      OS_VERS=$(lsb_release -sr)
+  elif [ -f /etc/lsb-release ]; then
+    # Para algunas versiones de Debian sin el comando lsb_release
+      . /etc/lsb-release
+      OS_NAME=$DISTRIB_ID
+      OS_VERS=$DISTRIB_RELEASE
+  elif [ -f /etc/debian_version ]; then
+    # Para versiones viejas de Debian.
+      OS_NAME=Debian
+      OS_VERS=$(cat /etc/debian_version)
+  else
+    # Para el viejo uname (También funciona para BSD)
+      OS_NAME=$(uname -s)
+      OS_VERS=$(uname -r)
+  fi
+
+if [ $OS_VERS == "7" ]; then
+
+  echo ""
+  echo "----------------------------------------------------------------------------------------"
+  echo "  Iniciando el script de preparación del container de Debian 7 (Wheezy) para NATear..."
+  echo "----------------------------------------------------------------------------------------"
+  echo ""
+
+  echo ""
+  echo "  Comandos para Debian 7 todavía no preparados. Prueba ejecutar el script en otra versión de Debian."
+  echo ""
+
+elif [ $OS_VERS == "8" ]; then
+
+  echo ""
+  echo "----------------------------------------------------------------------------------------"
+  echo "  Iniciando el script de preparación del container de Debian 8 (Jessie) para NATear..."
+  echo "----------------------------------------------------------------------------------------"
+  echo ""
+
+  echo ""
+  echo "  Comandos para Debian 8 todavía no preparados. Prueba ejecutar el script en otra versión de Debian."
+  echo ""
+
+elif [ $OS_VERS == "9" ]; then
+
+  echo ""
+  echo "-----------------------------------------------------------------------------------------"
+  echo "  Iniciando el script de preparación del container de Debian 9 (Stretch) para NATear..."
+  echo "-----------------------------------------------------------------------------------------"
+  echo ""
+
+  echo ""
+  echo "  Comandos para Debian 9 todavía no preparados. Prueba ejecutar el script en otra versión de Debian."
+  echo ""
+
+elif [ $OS_VERS == "10" ]; then
+
+  echo ""
+  echo "-----------------------------------------------------------------------------------------"
+  echo "  Iniciando el script de preparación del container de Debian 10 (Buster) para NATear..."
+  echo "-----------------------------------------------------------------------------------------"
+  echo ""
+
+  echo ""
+  echo "  Comandos para Debian 10 todavía no preparados. Prueba ejecutar el script en otra versión de Debian."
+  echo ""
+
+elif [ $OS_VERS == "11" ]; then
+
+  echo ""
+  echo "-------------------------------------------------------------------------------------------"
+  echo "  Iniciando el script de preparación del container de Debian 11 (Bullseye) para NATear..."
+  echo "-------------------------------------------------------------------------------------------"
+  echo ""
+
+  echo ""
+  echo "  Habilitando el forwarding entre interfaces..."
+  echo ""
+  cp /etc/sysctl.conf /etc/sysctl.conf.bak
+  sed -i -e 's|#net.ipv4.ip_forward=1|net.ipv4.ip_forward=1|g' /etc/sysctl.conf
+
+fi
+
+
+
