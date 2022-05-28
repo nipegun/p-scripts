f88b12cccdf9b978a23a9cb474b0faadc887f810
NiPeGun <nipegun@gmail.com>
2022-05-28 21:35:35 +0200

    Create PVE-RedInterna-CrearNueva.sh

---
+++ PVE-RedInterna-CrearNueva.sh
@@ -0,0 +1,20 @@
+#!/bin/bash
+
+# Pongo a disposición pública este script bajo el término de "software de dominio público".
+# Puedes hacer lo que quieras con él porque es libre de verdad; no libre con condiciones como las licencias GNU y otras patrañas similares.
+# Si se te llena la boca hablando de libertad entonces hazlo realmente libre.
+# No tienes que aceptar ningún tipo de términos de uso o licencia para utilizarlo o modificarlo porque va sin CopyLeft.
+
+# ----------
+#  Script de NiPeGun para crear una red interna con NAT en proxmox
+# ----------
+
+echo ""                           >> /etc/network/interfaces
+echo "auto vmbr20"                >> /etc/network/interfaces
+echo "  iface vmbr20 inet static" >> /etc/network/interfaces
+echo "  bridge_ports none"        >> /etc/network/interfaces
+echo "  bridge_stp off"           >> /etc/network/interfaces
+echo "  bridge_fd 0"              >> /etc/network/interfaces
+echo "#RedInterna"                >> /etc/network/interfaces
+systemctl restart networking
+
