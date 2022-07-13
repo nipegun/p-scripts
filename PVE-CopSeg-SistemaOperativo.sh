1c5d8cd79b76bd0459225744a9722b9cb73a52d9
NiPeGun <nipegun@gmail.com>
2022-07-13 17:55:56 +0200

    Update PVE-CopSeg-SistemaOperativo.sh

--- PVE-CopSeg-SistemaOperativo.sh
+++ PVE-CopSeg-SistemaOperativo.sh
@@ -43,8 +43,17 @@ echo ""
 
 # Ejecutar copia de seguridad de la base de datos
   echo ""
-  echo "  Haciendo copia de seguridad de la base de datos..."
+  echo "  Haciendo copia de seguridad de la base de datos Proxmox Cluster File System (pmxcfs)..."
   echo ""
+  # https://hacks4geeks.com/entendiendo-el-sistema-de-archivos-del-culster-de-proxmox/
+  # Comprobar si el paquete sqlite3 está instalado. Si no lo está, instalarlo.
+    if [[ $(dpkg-query -s sqlite3 2>/dev/null | grep installed) == "" ]]; then
+      echo ""
+      echo "    sqlite3 no está instalado. Iniciando su instalación..."
+      echo ""
+      apt-get -y update > /dev/null && apt-get -y install sqlite3
+      echo ""
+    fi
   vEstadoDeLaBaseDeDatosDePVE=$(sqlite3 /var/lib/pve-cluster/config.db "PRAGMA integrity_check;")
   if [ $vEstadoDeLaBaseDeDatosDePVE == "ok" ]; then
     echo ""
@@ -57,7 +66,7 @@ echo ""
     # Comprobar si el paquete sqlformat está instalado. Si no lo está, instalarlo.
       if [[ $(dpkg-query -s sqlformat 2>/dev/null | grep installed) == "" ]]; then
         echo ""
-        echo "  sqlformat no está instalado. Iniciando su instalación..."
+        echo "    sqlformat no está instalado. Iniciando su instalación..."
         echo ""
         apt-get -y update > /dev/null
         apt-get -y install sqlformat
@@ -78,73 +87,15 @@ echo ""
     sqlite3 /var/lib/pve-cluster/config.db ".dump" | sed -e 's|^ROLLBACK;\( -- due to errors\)*$|COMMIT;|g' | sqlite3 /var/lib/pve-cluster/config.db.dump-before-rollback
   fi
 
-# Crear copia de seguridad del tree
-  sqlite3 /var/lib/pve-cluster/config.db 'select * from tree;' > /CopSegInt/$vFechaDeEjec/BD/tree.txt
+  # Crear copia de seguridad del tree
+    echo ""
+    echo "    Exportando la tabla tree de la base de datos hacía el archivo tree.txt..."
+    echo ""
+    sqlite3 /var/lib/pve-cluster/config.db 'select * from tree;' > /CopSegInt/$vFechaDeEjec/BD/tree.txt
 
-
-# Crear copia de seguridad de los archivos de dentro de la base de datos
-  # /etc/pve/firewall/
-    /etc/pve/firewall/cluster.fw                  # Firewall configuration applied to all nodes
-    /etc/pve/firewall/*.fw                        # Firewall configuration for individual nodes
-    /etc/pve/firewall/*.fw                        # Firewall configuration for VMs and containers
-  # /etc/pve/ha/
-    /etc/pve/ha/crm_commands                      # Displays HA operations that are currently being carried out by the CRM
-    /etc/pve/ha/manager_status                    # JSON-formatted information regarding HA services on the cluster
-    /etc/pve/ha/resources.cfg                     # Resources managed by high availability, and their current state
-  # /etc/pve/local/       (Enlace simbólico a /etc/pve/nodes/<LOCAL_HOST_NAME>)
-    #
-  # /etc/pve/lxc/         (Enlace simbólico a /etc/pve/nodes/<LOCAL_HOST_NAME>/lxc/)
-    #
-  # /etc/pve/nodes/
-    # Obtener el nombre del nodo
-      vNombreNodo=$(ls /etc/pve/nodes/ | cut -d' ' -f1)
-    /etc/pve/nodes/$vNombreNodo/lxc/*.conf              # VM configuration data for LXC containers
-    /etc/pve/nodes/$vNombreNodo/openvz/*                # Prior to PVE 4.0, used for container configuration data (deprecated, removed soon)
-    /etc/pve/nodes/$vNombreNodo/priv/*
-    /etc/pve/nodes/$vNombreNodo/qemu-server/*.conf      # VM configuration data for KVM VMs
-    /etc/pve/nodes/$vNombreNodo/pve-ssl.key             # Private SSL key for pve-ssl.pem
-    /etc/pve/nodes/$vNombreNodo/pve-ssl.pem             # Public SSL certificate for web server (signed by cluster CA)
-    /etc/pve/nodes/$vNombreNodo/pveproxy-ssl.key        # Private SSL key for pveproxy-ssl.pem (optional)
-    /etc/pve/nodes/$vNombreNodo/pveproxy-ssl.pem        # Public SSL certificate (chain) for web server (optional override for pve-ssl.pem)
-    /etc/pve/nodes/$vNombreNodo/config                  # Node-specific configuration
-  # /etc/pve/openvz/      (Enlace simbólico a /etc/pve/nodes/<LOCAL_HOST_NAME>/openvz/ (deprecated, removed soon))
-    #
-  # /etc/pve/priv/
-    /etc/pve/priv/acme/*
-    /etc/pve/priv/lock/*                  # Lock files used by various services to ensure safe cluster-wide operations
-    /etc/pve/priv/storage/*.pw            # Contains the password of a storage in plain text
-    /etc/pve/priv/authkey.key             # Private key used by ticket system
-    /etc/pve/priv/authorized_keys         # SSH keys of cluster members for authentication
-    /etc/pve/priv/ceph*                   # Ceph authentication keys and associated capabilities
-    /etc/pve/priv/known_hosts             # SSH keys of the cluster members for verification
-    /etc/pve/priv/pve-root-ca.key         # Private key of cluster CA
-    /etc/pve/priv/pve-root-ca.srl
-    /etc/pve/priv/shadow.cfg              # Shadow password file for PVE Realm users
-    /etc/pve/priv/token.cfg               # API token secrets of all tokens
-    /etc/pve/priv/tfa.cfg                 # Base64-encoded two-factor authentication configuration
-  # /etc/pve/qemu-server/ (Enlace simbólico a /etc/pve/nodes/<LOCAL_HOST_NAME>/qemu-server/)    
-    #
-  # /etc/pve/sdn/
-    /etc/pve/sdn/*           # Shared configuration files for Software Defined Networking (SDN)
-  # /etc/pve/virtual-guest/
-    /etc/pve/virtual-guest/cpu-models.conf # For storing custom CPU models
-  # /etc/pve/*
-    /etc/pve/.clusterlog      # Cluster log (last 50 entries)
-    /etc/pve/.debug
-    /etc/pve/.members         # Info about cluster members
-    /etc/pve/.rrd             # RRD data (most recent entries)
-    /etc/pve/.version         # File versions (to detect file modifications)
-    /etc/pve/.vmlist          # List of all VMs
-    /etc/pve/authkey.pub      # Public key used by the ticket system
-    /etc/pve/ceph.conf        # Ceph configuration file (note: /etc/ceph/ceph.conf is a symbolic link to this)
-    /etc/pve/corosync.conf    # Corosync cluster configuration file (prior to Proxmox VE 4.x, this file was called cluster.conf)
-    /etc/pve/datacenter.cfg   # Proxmox VE data center-wide configuration (keyboard layout, proxy, …)
-    /etc/pve/domains.cfg      # Proxmox VE authentication domains
-    /etc/pve/pve-root-ca.pem  # Public certificate of cluster CA
-    /etc/pve/pve-www.key      # Private key used for generating CSRF tokens
-    /etc/pve/status.cfg       # Proxmox VE external metrics server configuration
-    /etc/pve/replication.conf
-    /etc/pve/storage.cfg      # Proxmox VE storage configuration
-    /etc/pve/user.cfg         # Proxmox VE access control configuration (users/groups/…)
-    /etc/pve/vzdump.cron      # Cluster-wide vzdump backup-job schedule
-
+  # Crear copia de seguridad de los archivos de dentro de la base de datos
+    echo ""
+    echo "    Creando copia de seguridad de los archivos de dentro de la base de datos..."
+    echo ""
+    mkdir -p /CopSegInt/$vFechaDeEjec/BD/etc/pve/ 2> /dev/null
+    cp -rfL /etc/pve/. /CopSegInt/$vFechaDeEjec/BD/etc/pve/
