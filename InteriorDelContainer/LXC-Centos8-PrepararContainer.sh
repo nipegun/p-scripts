d4bbc351d98de452da002744421d87a36d444666
NiPeGun <nipegun@gmail.com>
2022-09-25 11:21:52 +0200

    Update LXC-Centos8-PrepararContainer.sh

--- InteriorDelContainer/LXC-Centos8-PrepararContainer.sh
+++ InteriorDelContainer/LXC-Centos8-PrepararContainer.sh
@@ -1,5 +1,6 @@
 sed -i -e "s|mirrorlist=|#mirrorlist=|g" /etc/yum.repos.d/CentOS-*
 sed -i -e "s|#baseurl=http://mirror.centos.org|baseurl=http://vault.centos.org|g" /etc/yum.repos.d/CentOS-*
+echo "vm.swappiness=0" >> /etc/sysctl.d/swappiness.conf
 yum -y update
 yum -y upgrade
 
