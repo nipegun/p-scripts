f3c294a828233037cdeaaf764f6ecf3030972942
NiPeGun <nipegun@gmail.com>
2023-01-25 22:27:10 +0100

    Create PVE-Telegram-NotificarTexto.sh

---
+++ PVE-Telegram-NotificarTexto.sh
@@ -0,0 +1,33 @@
+#!/bin/bash
+
+# Pongo a disposición pública este script bajo el término de "software de dominio público".
+# Puedes hacer lo que quieras con él porque es libre de verdad; no libre con condiciones como las licencias GNU y otras patrañas similares.
+# Si se te llena la boca hablando de libertad entonces hazlo realmente libre.
+# No tienes que aceptar ningún tipo de términos de uso o licencia para utilizarlo o modificarlo porque va sin CopyLeft.
+
+# ----------
+#  Script de NiPeGun para notificar el cambio de IP WAN de un servidor Proxmox
+#
+#  Este script no puede ser ejecutado remotamente. Debe ser ejecutado desde los p-scripts.
+#
+#  El script contempla la existencia de dos archivos en dos rutas específicas:
+#    /root/scripts/Telegram/TokenDelBot.txt (Con el token del bot que enviará el mensaje)
+#    /root/scripts/Telegram/IdChat.txt      (Con el id del chat al que enviar el mensaje de Telegram)
+# ----------
+
+vFecha=$(date +%Y/%m/%d@%T)
+
+vColorAzul="\033[0;34m"
+vColorAzulClaro="\033[1;34m"
+vColorVerde='\033[1;32m'
+vColorRojo='\033[1;31m'
+vFinColor='\033[0m'
+
+vMensaje="$1"
+
+# Notificar por Telegram
+  vTokenDelBot=$(cat /root/scripts/Telegram/TokenDelBot.txt)
+  vIdChat=$(cat /root/scripts/Telegram/IdChat.txt)
+  /root/scripts/d-scripts/Telegram-EnviarTexto.sh  "$vTokenDelBot" "$vIdChat" "$vMensaje"
+
+
