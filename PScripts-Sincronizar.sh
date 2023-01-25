3650419df1ecedce4c977f69a2e78db5ff3413bf
NiPeGun <nipegun@gmail.com>
2023-01-25 21:45:55 +0100

    Update PScripts-Sincronizar.sh

--- PScripts-Sincronizar.sh
+++ PScripts-Sincronizar.sh
@@ -5,24 +5,25 @@
 # Si se te llena la boca hablando de libertad entonces hazlo realmente libre.
 # No tienes que aceptar ningún tipo de términos de uso o licencia para utilizarlo o modificarlo porque va sin CopyLeft.
 
-#----------------------------------------------------------------------------------------------------
+# ----------
 #  Script de NiPeGun para sincronizar los p-scripts
 #
 #  Ejecución remota:
 #  curl -s https://raw.githubusercontent.com/nipegun/p-scripts/master/PScripts-Sincronizar.sh | bash
-#----------------------------------------------------------------------------------------------------
+# ----------
 
-ColorRojo='\033[1;31m'
-ColorVerde='\033[1;32m'
-FinColor='\033[0m'
+vColorAzul="\033[0;34m"
+vColorAzulClaro="\033[1;34m"
+vColorVerde='\033[1;32m'
+vColorRojo='\033[1;31m'
+vFinColor='\033[0m'
 
 # Comprobar si el paquete wget está instalado. Si no lo está, instalarlo.
 if [[ $(dpkg-query -s wget 2>/dev/null | grep installed) == "" ]]; then
     echo ""
-    echo "  wget no está instalado. Iniciando su instalación..."
+    echo -e "${vColorRojo}  wget no está instalado. Iniciando su instalación...${vFinColor}"
     echo ""
-    apt-get -y update
-    apt-get -y install wget
+    apt-get -y update && apt-get -y install wget
     echo ""
 fi
 
@@ -31,8 +32,8 @@ wget -q --tries=10 --timeout=20 --spider https://github.com
   if [[ $? -eq 0 ]]; then
     echo ""
     echo "---------------------------------------------------------"
-    echo -e "  ${ColorVerde}Sincronizando los p-scripts con las últimas versiones${FinColor}"
-    echo -e "  ${ColorVerde} y descargando nuevos p-scripts si es que existen...${FinColor}"
+    echo -e "  ${vColorAzulClaro}Sincronizando los p-scripts con las últimas versiones${vFinColor}"
+    echo -e "  ${vColorAzulClaro} y descargando nuevos p-scripts si es que existen...${vFinColor}"
     echo "---------------------------------------------------------"
     echo ""
     rm /root/scripts/p-scripts -R 2> /dev/null
@@ -41,7 +42,7 @@ wget -q --tries=10 --timeout=20 --spider https://github.com
     ## Comprobar si el paquete git está instalado. Si no lo está, instalarlo.
        if [[ $(dpkg-query -s git 2>/dev/null | grep installed) == "" ]]; then
          echo ""
-         echo "  git no está instalado. Iniciando su instalación..."
+         echo -e "${vColorRojo}    wget no está instalado. Iniciando su instalación...${vFinColor}"
          echo ""
          apt-get -y update > /dev/null
          apt-get -y install git
@@ -54,13 +55,13 @@ wget -q --tries=10 --timeout=20 --spider https://github.com
     /root/scripts/p-scripts/PScripts-CrearAlias.sh
     echo ""
     echo "-----------------------------------------"
-    echo -e "  ${ColorVerde}p-scripts sincronizados correctamente${FinColor}"
+    echo -e "  ${vColorVerde}p-scripts sincronizados correctamente${vFinColor}"
     echo "-----------------------------------------"
     echo ""
   else
     echo ""
     echo "---------------------------------------------------------------------------------------------------"
-    echo -e "${ColorRojo}No se pudo iniciar la sincronización de los p-scripts porque no se detectó conexión a Internet.${FinColor}"
+    echo -e "${vColorRojo}No se pudo iniciar la sincronización de los p-scripts porque no se detectó conexión a Internet.${vFinColor}"
     echo "---------------------------------------------------------------------------------------------------"
     echo ""
   fi
