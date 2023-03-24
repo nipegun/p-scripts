0a36fa13dc185823ad26dbbf78f76c61bf7ccb4d
NiPeGun <nipegun@gmail.com>
2023-03-24 14:27:43 +0100

    Update and rename PVE-CopSeg-Importar-IDs.sh to PVE-CopSeg-IDs-Importar.sh

--- PVE-CopSeg-IDs-Importar.sh
+++ PVE-CopSeg-IDs-Importar.sh
@@ -9,7 +9,7 @@
 #  Script de NiPeGun para importar en Proxmox todas las copias de seguridad de contenedores y máquinas virtuales que haya en una carpeta dada
 #
 #  Ejecución remota:
-#    curl -s https://raw.githubusercontent.com/nipegun/p-scripts/master/PVE-CopSeg-Importar-IDs.sh | bash
+#    curl -s https://raw.githubusercontent.com/nipegun/p-scripts/master/PVE-CopSeg-IDs-Importar.sh | bash
 # ----------
 
 # Modificar sólo esto antes de ejecutar el script
