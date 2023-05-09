3d3528008767984b38f9f09e5268a964133ca255
NiPeGun <nipegun@gmail.com>
2023-05-09 16:48:09 +0200

    Update Kernels-Instalados-Mostrar.sh

--- Kernels-Instalados-Mostrar.sh
+++ Kernels-Instalados-Mostrar.sh
@@ -13,7 +13,7 @@
 # ----------
 
 # Determinar kernels instalados
-  apt list --installed | grep pve-kernel | grep -v all | cut -d'/' -f1 &> /tmp/KernelsInstalados.txt
+  apt list --installed | grep pve-kernel | grep -v all | cut -d'/' -f1 &2> /tmp/KernelsInstalados.txt
   cat /tmp/KernelsInstalados.txt | grep pve-kernel > /tmp/KernelsInstalados.txt
 # Mostrar los kernels instalados
   echo ""
