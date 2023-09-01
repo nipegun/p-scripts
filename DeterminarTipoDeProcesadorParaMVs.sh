0dae2239c7cdeebdedc4c4308754c9f9bad2bf23
NiPeGun <nipegun@gmail.com>
2023-09-01 02:41:19 +0200

    Update DeterminarTipoDeProcesadorParaMVs.sh

--- DeterminarTipoDeProcesadorParaMVs.sh
+++ DeterminarTipoDeProcesadorParaMVs.sh
@@ -24,7 +24,7 @@
 # ----------
 
 vFlags=$(cat /proc/cpuinfo | grep flags | head -n 1 | cut -d: -f2)
- 
+
 vSupports_v2='awk "/cx16/&&/lahf/&&/popcnt/&&/sse4_1/&&/sse4_2/&&/ssse3/ {found=1} END {exit !found}"'
 vSupports_v3='awk "/avx/&&/avx2/&&/bmi1/&&/bmi2/&&/f16c/&&/fma/&&/abm/&&/movbe/&&/xsave/ {found=1} END {exit !found}"'
 vSupports_v4='awk "/avx512f/&&/avx512bw/&&/avx512cd/&&/avx512dq/&&/avx512vl/ {found=1} END {exit !found}"'
