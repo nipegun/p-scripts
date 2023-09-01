9751bfa55a1e049e838d8b33d7630aa3efdac988
NiPeGun <nipegun@gmail.com>
2023-09-01 03:57:29 +0200

    Update DeterminarTipoDeProcesadorParaMVs.sh

--- DeterminarTipoDeProcesadorParaMVs.sh
+++ DeterminarTipoDeProcesadorParaMVs.sh
@@ -25,20 +25,13 @@
 
 vFlags=$(cat /proc/cpuinfo | grep flags | head -n 1 | cut -d: -f2 | sed 's- -\n-g' | sort | tr '\n' ' ')
 
-vSupports_v2='awk "/cx16/&&/lahf_lm/&&/pni/&&/popcnt/&&/sse4_1/&&/sse4_2/&&/ssse3/            {found=1} END {exit !found}"'
+vSupports_v2='awk     "/cx16/&&/lahf_lm/&&/pni/&&/popcnt/&&/sse4_1/&&/sse4_2/&&/ssse3/        {found=1} END {exit !found}"'
 vSupports_v2_AES='awk "/cx16/&&/lahf_lm/&&/pni/&&/popcnt/&&/sse4_1/&&/sse4_2/&&/ssse3/&&/aes/ {found=1} END {exit !found}"'
-vSupports_v3='awk "/avx/&&/avx2/&&/bmi1/&&/bmi2/&&/f16c/&&/fma/&&/abm/&&/movbe/&&/xsave/      {found=1} END {exit !found}"'
-vSupports_v4='awk "/avx512f/&&/avx512bw/&&/avx512cd/&&/avx512dq/&&/avx512vl/                  {found=1} END {exit !found}"'
+#vSupports_v3='awk     "/avx/&&/avx2/&&/bmi1/&&/bmi2/&&/f16c/&&/fma/&&/abm/&&/movbe/&&/xsave/  {found=1} END {exit !found}"'
+#vSupports_v4='awk     "/avx512f/&&/avx512bw/&&/avx512cd/&&/avx512dq/&&/avx512vl/              {found=1} END {exit !found}"'
 
-BEGIN {
-    while (!/flags/) if (getline < "/proc/cpuinfo" != 1) exit 1
-    if (/lm/&&/cmov/&&/cx8/&&/fpu/&&/fxsr/&&/mmx/&&/syscall/&&/sse2/) level = 1
-    if (level == 1 && /cx16/&&/lahf/&&/popcnt/&&/sse4_1/&&/sse4_2/&&/ssse3/) level = 2
-    if (level == 2 && /avx/&&/avx2/&&/bmi1/&&/bmi2/&&/f16c/&&/fma/&&/abm/&&/movbe/&&/xsave/) level = 3
-    if (level == 3 && /avx512f/&&/avx512bw/&&/avx512cd/&&/avx512dq/&&/avx512vl/) level = 4
-    if (level > 0) { print "CPU supports x86-64-v" level; exit level + 1 }
-    exit 1
-}
+vSupports_v3='awk     "/cx16/&&/lahf_lm/&&/pni/&&/popcnt/&&/sse4_1/&&/sse4_2/&&/ssse3/&&/aes/avx/&&/avx2/&&/bmi1/&&/bmi2/&&/f16c/&&/fma/&&/abm/&&/movbe/&&/xsave/  {found=1} END {exit !found}"'
+vSupports_v4='awk     "/cx16/&&/lahf_lm/&&/pni/&&/popcnt/&&/sse4_1/&&/sse4_2/&&/ssse3/&&/aes/avx/&&/avx2/&&/bmi1/&&/bmi2/&&/f16c/&&/fma/&&/abm/&&/movbe/&&/xsave/avx512f/&&/avx512bw/&&/avx512cd/&&/avx512dq/&&/avx512vl/              {found=1} END {exit !found}"'
 
 echo ""
 echo "  El procesador de este Proxmox permite asignar a las MVs los siguientes tipos de CPU x86-64:"
