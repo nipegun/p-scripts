ccd15da2641f16eb094533e858e95782f1ebcc05
NiPeGun <nipegun@gmail.com>
2023-09-01 04:09:46 +0200

    Update DeterminarTipoDeProcesadorParaMVs.sh

--- DeterminarTipoDeProcesadorParaMVs.sh
+++ DeterminarTipoDeProcesadorParaMVs.sh
@@ -23,25 +23,22 @@
 #   flags: cx16, lahf-lm, popcnt, pni, sse4.1, sse4.2, ssse3, aes, avx, avx2, bmi1, bmi2, f16c, fma, movbe, xsave, avx512f, avx512bw, avx512cd, avx512dq, avx512vl.
 # ----------
 
-vFlags=$(cat /proc/cpuinfo | grep flags | head -n 1 | cut -d: -f2 | sed 's- -\n-g' | sort | tr '\n' ' ')
+vTodasLasFlagsDelProc=$(cat /proc/cpuinfo | grep flags | head -n 1 | cut -d: -f2 | sed 's- -\n-g' | sort | tr '\n' ' ')
 
-vSupports_v2='awk     "/cx16/&&/lahf_lm/&&/pni/&&/popcnt/&&/sse4_1/&&/sse4_2/&&/ssse3/        {found=1} END {exit !found}"'
-vSupports_v2_AES='awk "/cx16/&&/lahf_lm/&&/pni/&&/popcnt/&&/sse4_1/&&/sse4_2/&&/ssse3/&&/aes/ {found=1} END {exit !found}"'
-#vSupports_v3='awk     "/avx/&&/avx2/&&/bmi1/&&/bmi2/&&/f16c/&&/fma/&&/abm/&&/movbe/&&/xsave/  {found=1} END {exit !found}"'
-#vSupports_v4='awk     "/avx512f/&&/avx512bw/&&/avx512cd/&&/avx512dq/&&/avx512vl/              {found=1} END {exit !found}"'
-
-vSupports_v3='awk     "/cx16/&&/lahf_lm/&&/pni/&&/popcnt/&&/sse4_1/&&/sse4_2/&&/ssse3/&&/aes/&&/avx/&&/avx2/&&/bmi1/&&/bmi2/&&/f16c/&&/fma/&&/abm/&&/movbe/&&/xsave/  {found=1} END {exit !found}"'
-vSupports_v4='awk     "/cx16/&&/lahf_lm/&&/pni/&&/popcnt/&&/sse4_1/&&/sse4_2/&&/ssse3/&&/aes/&&/avx/&&/avx2/&&/bmi1/&&/bmi2/&&/f16c/&&/fma/&&/abm/&&/movbe/&&/xsave/&&/avx512f/&&/avx512bw/&&/avx512cd/&&/avx512dq/&&/avx512vl/ {found=1} END {exit !found}"'
+vFlags_v2='awk     "/cx16/&&/lahf_lm/&&/pni/&&/popcnt/&&/sse4_1/&&/sse4_2/&&/ssse3/                                                                                                                                          {found=1} END {exit !found}"'
+vFlags_v2_AES='awk "/cx16/&&/lahf_lm/&&/pni/&&/popcnt/&&/sse4_1/&&/sse4_2/&&/ssse3/&&/aes/                                                                                                                                   {found=1} END {exit !found}"'
+vFlags_v3='awk     "/cx16/&&/lahf_lm/&&/pni/&&/popcnt/&&/sse4_1/&&/sse4_2/&&/ssse3/&&/aes/&&/avx/&&/avx2/&&/bmi1/&&/bmi2/&&/f16c/&&/fma/&&/abm/&&/movbe/&&/xsave/                                                            {found=1} END {exit !found}"'
+vFlags_v4='awk     "/cx16/&&/lahf_lm/&&/pni/&&/popcnt/&&/sse4_1/&&/sse4_2/&&/ssse3/&&/aes/&&/avx/&&/avx2/&&/bmi1/&&/bmi2/&&/f16c/&&/fma/&&/abm/&&/movbe/&&/xsave/&&/avx512f/&&/avx512bw/&&/avx512cd/&&/avx512dq/&&/avx512vl/ {found=1} END {exit !found}"'
 
 echo ""
 echo "  El procesador de este Proxmox permite asignar a las MVs los siguientes tipos de CPU x86-64:"
 echo ""
-echo "$vFlags" | eval $vSupports_v2     || exit 2 && echo "    x86-64-v2     (Flags de Intel Nehalem y AMD Opteron_G3, o superiores)"
-echo "$vFlags" | eval $vSupports_v2_AES || exit 3 && echo "    x86-64-v2-AES (Flags de Intel Westmere y AMD Opteron_G4, o superiores)"
-echo "$vFlags" | eval $vSupports_v3     || exit 4 && echo "    x86-64-v3     (Flags de Intel Broadwell y AMD EPYC, o superiores)"
-echo "$vFlags" | eval $vSupports_v4     || exit 5 && echo "    x86-64-v4     (Flags de Intel Skylake y AMD EPYC v4 Genoa, o superiores)"
+echo "$vTodasLasFlagsDelProc" | eval $vFlags_v2     || exit 2 && echo "    x86-64-v2     (Flags de Intel Nehalem y AMD Opteron_G3, o superiores)"
+echo "$vTodasLasFlagsDelProc" | eval $vFlags_v2_AES || exit 3 && echo "    x86-64-v2-AES (Flags de Intel Westmere y AMD Opteron_G4, o superiores)"
+echo "$vTodasLasFlagsDelProc" | eval $vFlags_v3     || exit 4 && echo "    x86-64-v3     (Flags de Intel Broadwell y AMD EPYC, o superiores)"
+echo "$vTodasLasFlagsDelProc" | eval $vFlags_v4     || exit 5 && echo "    x86-64-v4     (Flags de Intel Skylake y AMD Ryzen 7000, AMD EPYC v4 Genoa, o superiores)"
 
-vFlags-v2=$(    echo $vFlags | grep -E --color 'cx16|lahf_lm|pni|popcnt|sse4_1|sse4_2|ssse3')
-vFlags-v2-AES=$(echo $vFlags | grep -E --color 'cx16|lahf_lm|pni|popcnt|sse4_1|sse4_2|ssse3| aes ')
-vFlags-v3=$(    echo $vFlags | grep -E --color 'cx16|lahf_lm|pni|popcnt|sse4_1|sse4_2|ssse3| aes |avx|avx2|bmi1|bmi2|f16c|fma|movbe|xsave')
-vFlags-v4=$(    echo $vFlags | grep -E --color 'cx16|lahf_lm|pni|popcnt|sse4_1|sse4_2|ssse3| aes |avx|avx2|bmi1|bmi2|f16c|fma|movbe|xsave|avx512f|avx512bw|avx512cd|avx512dq|avx512vl')
+vFlags-v2=$(    echo $vTodasLasFlagsDelProc | grep -E --color 'cx16|lahf_lm|pni|popcnt|sse4_1|sse4_2|ssse3')
+vFlags-v2-AES=$(echo $vTodasLasFlagsDelProc | grep -E --color 'cx16|lahf_lm|pni|popcnt|sse4_1|sse4_2|ssse3| aes ')
+vFlags-v3=$(    echo $vTodasLasFlagsDelProc | grep -E --color 'cx16|lahf_lm|pni|popcnt|sse4_1|sse4_2|ssse3| aes |avx|avx2|bmi1|bmi2|f16c|fma|movbe|xsave')
+vFlags-v4=$(    echo $vTodasLasFlagsDelProc | grep -E --color 'cx16|lahf_lm|pni|popcnt|sse4_1|sse4_2|ssse3| aes |avx|avx2|bmi1|bmi2|f16c|fma|movbe|xsave|avx512f|avx512bw|avx512cd|avx512dq|avx512vl')
