e7749ac718df601bc7631bc22c11e6985bfc3646
NiPeGun <nipegun@gmail.com>
2025-01-20 02:31:24 +0100

    Update Preparar-PasarGráficaAContainer.sh

--- Preparar-PasarGráficaAContainer.sh
+++ Preparar-PasarGráficaAContainer.sh
@@ -1,44 +1,44 @@
 #!/bin/bash
 
-# Determinar la cantidad de gráficas que hay en el sistema
-  declare -a aTarjetas
-  aGPUs=$(ls -l /dev/dri | grep card | cut -d' ' -f13)
+# Crear el archivo .csv donde guardar la información de las tarjetas gráficas disponibles
 
-  # Imprimir el contenido del array
-    for vTarjeta in "${aGPUs[@]}"; do
-      echo "$vTarjeta"
-    done
+  # Determinar la cantidad de gráficas que hay en el sistema
+    aGPUs=($(ls -l /dev/dri | grep card | cut -d' ' -f13))
+    # Imprimir el contenido del array
+      #for vTarjeta in "${aGPUs[@]}"; do
+      #  echo "$vTarjeta"
+      #done
 
-# Determinar la ruta PCI de cada una de las tarjetas
-  aRutaPCITarjetas=($(ls -l /dev/dri/by-path/ | grep card | cut -d' ' -f10 | cut -d'-' -f2))
+  # Determinar la ruta PCI de cada una de las tarjetas
+    aRutaPCITarjetas=($(ls -l /dev/dri/by-path/ | grep card | cut -d' ' -f10 | cut -d'-' -f2))
+    # Imprimir el contenido del array
+      #for vRuta in "${aRutaPCITarjetas[@]}"; do
+      #  echo "$vRuta"
+      #done
 
-  # Imprimir el contenido del array
-    for vRuta in "${aRutaPCITarjetas[@]}"; do
-      echo "$vRuta"
-    done
+  # Determinar el dispositivo de render que le corresponde a cada una de las tarjetas
+    aDispoRenderTarjetas=($(ls -l /dev/dri/by-path/ | grep render | cut -d'/' -f2))
+    # Imprimir el contenido del array
+      #for vDispoRender in "${aDispoRenderTarjetas[@]}"; do
+      #  echo "$vDispoRender"
+     #done
 
-# Determinar el dispositivo de render que le corresponde a cada una de las tarjetas
-  aDispoRenderTarjetas=($(ls -l /dev/dri/by-path/ | grep render | cut -d'/' -f2))
+  # Crear el archivo .csv y escribir el encabezado
+    vArchivoCSV='/tmp/GPUs.csv'
+    echo "Tarjeta,DispoRender,RutaPCI" > "$vArchivoCSV"
 
-  # Imprimir el contenido del array
-    for vDispoRender in "${aDispoRenderTarjetas[@]}"; do
-      echo "$vDispoRender"
-    done
-
-# Crear el archivo .csv y escribir el encabezado
-  vArchivoCSV='/tmp/GPUs.csv'
-  echo "Tarjeta,DispoRender,RutaPCI" > $vArchivoCSV
-
-# Combinar la información de los arrays en el archivo CSV
-  for i in "${!aGPUs[@]}"; do
-    vTarjeta="${aGPUs[$i]}"
-    vRutaPCI="${aRutaPCITarjetas[$i]}"
-    vDispoRender="${aDispoRenderTarjetas[$i]}"
+  # Combinar la información de los arrays en el archivo CSV
+    for i in "${!aGPUs[@]}"; do
+      tarjeta="${aGPUs[$i]}"
+      rutaPCI="${aRutaPCITarjetas[$i]}"
+      dispoRender="${aDispoRenderTarjetas[$i]}"
   
-    # Escribir la fila en el archivo CSV
-    echo "${vTarjeta},${vDispoRender},${vRutaPCI}" >> "$vArchivoCSV"
-  done
+      # Escribir la fila en el archivo CSV
+      echo "${tarjeta},${dispoRender},${rutaPCI}" >> "$vArchivoCSV"
+    done
 
+  # Mostrar el contenido del archivo
+    cat "$vArchivoCSV"
 
 # Permitir al root mapear los grupos video y render
 
@@ -53,7 +53,6 @@
     echo "root:$vIDGrupoRender:1" >> /etc/subgid
 
 # Determinar cual es la GPU integrada y cual la pinchada
-  declare -a aGPUs
   aGPUs=$(ls -l /dev/dri)
 
 # listar los dispositivos relacionados con el Direct Rendering Infrastructure (DRI)
