3831fb37de0465b628fc8bff819c890ce1a80168
NiPeGun <nipegun@gmail.com>
2025-01-20 01:39:15 +0100

    Update Preparar-PasarGráficaAContainer.sh

--- Preparar-PasarGráficaAContainer.sh
+++ Preparar-PasarGráficaAContainer.sh
@@ -1,15 +1,14 @@
 #!/bin/bash
 
+# Permitir al root mapear los grupos video y render
+  # Encontrar el IDGroup de render (Normalmente 105)
+    vIDGrupoRender=$(cat /etc/group | grep ^render | cut -d':' -f3)
+  # Encontrar el IDGroup de video (Normalmente 44)
+    vIDGrupoVideo=$(cat /etc/group | grep ^video | cut -d':' -f3)
+  # Permitir al root mapear ambos grupos a nuevos groupsids
+    echo "root:$vIDGrupoVideo:1"  >> /etc/subgid
+    echo "root:$vIDGrupoRender:1" >> /etc/subgid
+
+
 # listar los dispositivos relacionados con el Direct Rendering Infrastructure (DRI)
   ls -l /dev/dri
-
-# Encontrar el IDGroup de render (Normalmente 105)
-  vIDGrupoRender=$(cat /etc/group | grep ^render | cut -d':' -f3)
-
-# Encontrar el IDGroup de video (Normalmente 44)
-  vIDGrupoVideo=$(cat /etc/group | grep ^video | cut -d':' -f3)
-
-# Agregar los números de los grupos al a subgid
-  echo "root:$vIDGrupoVideo:1" >> /etc/subgid
-  echo "root:$vIDGrupoRender:1" >> /etc/subgid
-  
