71f2c075fed1fa924621edb61f736ba14c4de18e
NiPeGun <nipegun@gmail.com>
2025-10-30 06:00:31 +0100

    Update PVE-Host-Nombre-Cambiar.sh

--- PVE-Host-Nombre-Cambiar.sh
+++ PVE-Host-Nombre-Cambiar.sh
@@ -53,38 +53,85 @@ if [ $# -ne $cCantArgumEsperados ]
     echo ""
     exit
   else
-    # Notificar
       # Obtener el nombre actual del host
-        vNombreActualDelHost=$(hostname)
+        vNombreActual=$(hostname)
+      # Guardar en una variable el nombre que se ha pasado por parámetro
+        vNombreNuevo="$1"
+        
+      # Verificar si el nodo está en un cluster
+
+      # Si está en un cluster, sacar el nodo del cluster
+        pvecm delnode <nombre_antiguo>
+
+      # Luego unirse nuevamente al cluster usando el nuevo nombre
+          pvecm add <ip-del-master>)
+
+      # Si estás en cluster
+        pvecm nodes
+
+    # Notificar
+
       echo ""
-      echo "    Cambiando el nombre del host de $vNombreActualDelHost a $1..."
+      echo "    Cambiando el nombre del host de $vNombreActual a $vNombreNuevo..."
       echo "    Dependiendo de la velocidad del procesador y de la unidad donde está instalado Proxmox, la operación puede tardar más de 10 minutos..."
       echo "    Al finalizar, se le preguntará al usuario si quiere reiniciar PVE. Es aconsejado hacerlo."
       echo ""
-      find /bin        -type f -exec sed -i -e "s|$vNombreActualDelHost|$1|g" {} \;
-      find /boot       -type f -exec sed -i -e "s|$vNombreActualDelHost|$1|g" {} \;
-      find /dev        -type f -exec sed -i -e "s|$vNombreActualDelHost|$1|g" {} \;
-      find /etc        -type f -exec sed -i -e "s|$vNombreActualDelHost|$1|g" {} \;
-      find /home       -type f -exec sed -i -e "s|$vNombreActualDelHost|$1|g" {} \;
-      find /lib        -type f -exec sed -i -e "s|$vNombreActualDelHost|$1|g" {} \;
-      find /lib64      -type f -exec sed -i -e "s|$vNombreActualDelHost|$1|g" {} \;
-      find /lost+found -type f -exec sed -i -e "s|$vNombreActualDelHost|$1|g" {} \;
-      find /media      -type f -exec sed -i -e "s|$vNombreActualDelHost|$1|g" {} \;
-      find /mnt        -type f -exec sed -i -e "s|$vNombreActualDelHost|$1|g" {} \;
-      find /opt        -type f -exec sed -i -e "s|$vNombreActualDelHost|$1|g" {} \;
-      find /root       -type f -exec sed -i -e "s|$vNombreActualDelHost|$1|g" {} \;
-      find /run        -type f -exec sed -i -e "s|$vNombreActualDelHost|$1|g" {} \;
-      find /sbin       -type f -exec sed -i -e "s|$vNombreActualDelHost|$1|g" {} \;
-      find /srv        -type f -exec sed -i -e "s|$vNombreActualDelHost|$1|g" {} \;
-      find /tmp        -type f -exec sed -i -e "s|$vNombreActualDelHost|$1|g" {} \;
-      find /usr        -type f -exec sed -i -e "s|$vNombreActualDelHost|$1|g" {} \;
-      find /var        -type f -exec sed -i -e "s|$vNombreActualDelHost|$1|g" {} \;
-      echo ""
-    # Notificar del cambio y pedir aceptar
-      echo ""
-      echo "    Se reemplazó el texto $vNombreActualDelHost por $1 en todos los archivos del sistema."
-      echo ""
-      read -p "      Presiona cualquier tecla reiniciar el sistema o presiona CTRL+C para saltar a la terminal"
-      shutdown -r now
+
+      # Parar servicios
+        systemctl stop pveproxy && systemctl stop pvedaemon && systemctl stop pve-cluster && systemctl stop corosync
+
+      # Hacer copia de seguridad de la base de datos
+        cp -fv /var/lib/pve-cluster/config.db /root/config.db.bak
+
+      # Modificar la base de datos
+        sqlite3 /var/lib/pve-cluster/config.db "UPDATE tree SET data = REPLACE(data, '$vNombreActual', '$vNombreNuevo');"
+        sqlite3 /var/lib/pve-cluster/config.db "UPDATE tree SET name = REPLACE(name, '$vNombreActual', '$vNombreNuevo');"
+
+      # Listar la modificación
+        sqlite3 /var/lib/pve-cluster/config.db "SELECT* FROM tree;"
+
+      # Modificar /etc/hosts
+        sed -i "s|$vNombreActual|$vNombreNuevo|g" /etc/hosts
+
+      # Cambiar el hostname
+        hostnamectl set-hostname "$vNombreNuevo"
+  
+      # Reiniciar servicios
+        systemctl start corosync && systemctl start pve-cluster && systemctl start pvedaemon && systemctl start pveproxy
+  
+      # Comprobar que el nodo se haya levantado con el nuevo nombre y proseguir con el renombrado del resto de archivos del sistema
+        if [ -d /etc/pve/nodes/"$vNombreNuevo"  ]; then
+          echo ""
+          echo "  La carpeta /etc/pve/nodes/"$vNombreNuevo" existe. El nodo se ha renombrado correctamente!"
+          echo ""
+          echo "    Iniciando el reemplazo de la cadena $vNombreActual por la cadena $vNombreNuevo en todos los archivos del sistema..."
+          echo ""
+          find /bin        -type f -exec sed -i -e "s|$vNombreActual|$vNombreNuevo|g" {} \;
+          find /boot       -type f -exec sed -i -e "s|$vNombreActual|$vNombreNuevo|g" {} \;
+          find /dev        -type f -exec sed -i -e "s|$vNombreActual|$vNombreNuevo|g" {} \;
+          find /etc        -type f -exec sed -i -e "s|$vNombreActual|$vNombreNuevo|g" {} \;
+          find /home       -type f -exec sed -i -e "s|$vNombreActual|$vNombreNuevo|g" {} \;
+          find /lib        -type f -exec sed -i -e "s|$vNombreActual|$vNombreNuevo|g" {} \;
+          find /lib64      -type f -exec sed -i -e "s|$vNombreActual|$vNombreNuevo|g" {} \;
+          find /lost+found -type f -exec sed -i -e "s|$vNombreActual|$vNombreNuevo|g" {} \;
+          find /media      -type f -exec sed -i -e "s|$vNombreActual|$vNombreNuevo|g" {} \;
+          find /mnt        -type f -exec sed -i -e "s|$vNombreActual|$vNombreNuevo|g" {} \;
+          find /opt        -type f -exec sed -i -e "s|$vNombreActual|$vNombreNuevo|g" {} \;
+          find /root       -type f -exec sed -i -e "s|$vNombreActual|$vNombreNuevo|g" {} \;
+          find /run        -type f -exec sed -i -e "s|$vNombreActual|$vNombreNuevo|g" {} \;
+          find /sbin       -type f -exec sed -i -e "s|$vNombreActual|$vNombreNuevo|g" {} \;
+          find /srv        -type f -exec sed -i -e "s|$vNombreActual|$vNombreNuevo|g" {} \;
+          find /tmp        -type f -exec sed -i -e "s|$vNombreActual|$vNombreNuevo|g" {} \;
+          find /usr        -type f -exec sed -i -e "s|$vNombreActual|$vNombreNuevo|g" {} \;
+          find /var        -type f -exec sed -i -e "s|$vNombreActual|$vNombreNuevo|g" {} \;
+          echo ""
+          # Notificar del cambio y pedir aceptar
+            echo ""
+            echo "    Se reemplazó el texto $vNombreActual por $vNombreNuevo en todos los archivos del sistema."
+            echo ""
+            read -p "      Presiona cualquier tecla reiniciar el sistema o presiona CTRL+C para saltar a la terminal"
+            shutdown -r now
+        fi
+
 fi
 
